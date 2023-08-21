60975223d43331d1e6bf1bc13c1dc0ca4c6af857
Veniamin-Leb <62715894+Veniamin-Leb@users.noreply.github.com>
2023-08-21 11:03:32 +0300

    Add files via upload

---
+++ assets/js/app.js
@@ -0,0 +1,6 @@
+$('.slider').slick({
+    infinite: true,
+    dots: true,
+    nextArrow: '<button type="button" class="slick-next"><img src="assets/IMG/next.svg"></button>',
+    prevArrow: '<button type="button" class="slick-prev"><img src="assets/IMG/prev.svg"></button>',
+});
